aef888523879bebba5fbc69b626899096ea77b3c
Ivan Salgado <101019855+ivanjsalgado@users.noreply.github.com>
2023-08-27 13:40:19 -0400

    Preliminary styling for ranking and leaderboard

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,10 +1,23 @@
-import Back from "../../assets/images/Back_edited.svg";
+import "./Leaderboard.scss";
 import BackAg from "../../assets/images/Back_Again.png";
+import { Link } from "react-router-dom";
+import Profile from "../../assets/images/Ivan Salgado  - Software Engineering - June Miami 2023.jpg";
+import Ranking from "../Ranking/Ranking";
 function Leaderboard() {
   return (
-    <div>
-      <img src={BackAg} alt="Back Icon" />
-      Leaderboard
+    <div className="leaderboard">
+      <div className="leaderboard__header">
+        <Link
+          style={{ "text-decoration": "none", display: "flex" }}
+          to={"/home"}
+        >
+          <img className="leaderboard__back" src={BackAg} alt="Back Icon" />
+        </Link>
+        <h1 className="leaderboard__heading">Leaderboard</h1>
+        <img className="leaderboard__photo" src={Profile} alt="User Profile" />
+      </div>
+      <Ranking />
+      <Ranking />
     </div>
   );
 }
